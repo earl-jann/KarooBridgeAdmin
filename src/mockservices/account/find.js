0903b118da6ed5683be4488f3edaa7be1c855c3b
earl-jann <earljanntormes@gmail.com>
2017-12-11 15:22:47 +0800

    Cleanup

--- src/mockservices/account/find.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-
-// Mock any GET request to /accounts
-// arguments for reply are (status, data, headers)
-Vue.$mockHttp.onGet('/accounts').reply(200, {
-  accounts: [
-    { id: 1, name: 'John Smith' },
-  ],
-});
